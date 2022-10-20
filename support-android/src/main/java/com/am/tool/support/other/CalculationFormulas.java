73912c5c8c961b4c26ba877c75799384d3d3f383
Alex <xiangzc@surperace.com>
2022-10-20 11:53:24 +0800

    [update] 增加屏幕坐标系点角度的更多获取方式。

--- support-android/src/main/java/com/am/tool/support/other/CalculationFormulas.java
+++ support-android/src/main/java/com/am/tool/support/other/CalculationFormulas.java
@@ -67,15 +67,8 @@ public class CalculationFormulas {
         return Math.sqrt((x - px) * (x - px) + (py - y) * (py - y));
     }
 
-    /**
-     * 计算在Android坐标系中，从(0, 0)到(x, y)连成线段，
-     * 该线段与X轴正方向顺时针形成的夹角角度（0~360）
-     *
-     * @param x 直角坐标系X轴坐标
-     * @param y 直角坐标系Y轴坐标
-     * @return 角度
-     */
-    public static double getDegrees(double x, double y) {
+    // 线段与X轴正方向顺时针形成的夹角角度（0~360）
+    private static double getDegreesXPositiveCW(double x, double y) {
         if (y == 0) {
             if (x >= 0)
                 return 0;
@@ -114,17 +107,245 @@ public class CalculationFormulas {
         return 0;
     }
 
+    // 线段与X轴正方向逆时针形成的夹角角度（0~360）
+    private static double getDegreesXPositiveCCW(double x, double y) {
+        if (y == 0) {
+            if (x >= 0)
+                return 0;
+            return 180;
+        }
+        if (x == 0) {
+            if (y > 0)
+                return 270;
+            return 90;
+        }
+        if (x > 0 && y < 0) {
+            // 右上
+            if (x == -y)
+                return 45;
+            return Math.toDegrees(Math.atan(-y / x));
+        }
+        if (x < 0 && y < 0) {
+            // 左上
+            if (x == y)
+                return 135;
+            return Math.toDegrees(Math.PI - Math.atan(y / x));
+        }
+        if (x < 0 && y > 0) {
+            // 左下
+            if (-x == y)
+                return 225;
+
+            return Math.toDegrees(Math.PI + Math.atan(-y / x));
+        }
+        if (x > 0 && y > 0) {
+            // 右下
+            if (x == y)
+                return 315;
+            return Math.toDegrees(Math.PI * 2 - Math.atan(y / x));
+        }
+        return 0;
+    }
+
+    // 线段与X轴反方向顺时针形成的夹角角度（0~360）
+    private static double getDegreesXNegativeCW(double x, double y) {
+        if (y == 0) {
+            if (x <= 0)
+                return 0;
+            return 180;
+        }
+        if (x == 0) {
+            if (y < 0)
+                return 90;
+            return 270;
+        }
+        if (x < 0 && y < 0) {
+            // 左上
+            if (x == y)
+                return 45;
+            return Math.toDegrees(Math.atan(y / x));
+        }
+        if (x > 0 && y < 0) {
+            // 右上
+            if (x == -y)
+                return 135;
+            return Math.toDegrees(Math.PI - Math.atan(-y / x));
+        }
+        if (x > 0 && y > 0) {
+            // 右下
+            if (x == y)
+                return 225;
+            return Math.toDegrees(Math.PI + Math.atan(y / x));
+        }
+        if (x < 0 && y > 0) {
+            // 左下
+            if (-x == y)
+                return 315;
+            return Math.toDegrees(Math.PI * 2 - Math.atan(-y / x));
+        }
+        return 0;
+    }
+
+    // 线段与X轴反方向逆时针形成的夹角角度（0~360）
+    private static double getDegreesXNegativeCCW(double x, double y) {
+        if (y == 0) {
+            if (x <= 0)
+                return 0;
+            return 180;
+        }
+        if (x == 0) {
+            if (y > 0)
+                return 90;
+            return 270;
+        }
+        if (x < 0 && y > 0) {
+            // 左下
+            if (-x == y)
+                return 45;
+            return Math.toDegrees(Math.atan(-y / x));
+        }
+        if (x > 0 && y > 0) {
+            // 右下
+            if (x == y)
+                return 135;
+            return Math.toDegrees(Math.PI - Math.atan(y / x));
+        }
+        if (x > 0 && y < 0) {
+            // 右上
+            if (x == -y)
+                return 225;
+            return Math.toDegrees(Math.PI + Math.atan(-y / x));
+        }
+        if (x < 0 && y < 0) {
+            // 左上
+            if (x == y)
+                return 315;
+            return Math.toDegrees(Math.PI * 2 - Math.atan(y / x));
+        }
+        return 0;
+    }
+
+    // 线段与Y轴正方向顺时针形成的夹角角度（0~360）
+    private static double getDegreesYPositiveCW(double x, double y) {
+        double degrees = 90 + getDegreesXPositiveCW(x, y);
+        while (degrees < 0) {
+            degrees += 360;
+        }
+        while (degrees > 360) {
+            degrees -= 360;
+        }
+        return degrees;
+    }
+
+    // 线段与X轴反方向顺时针形成的夹角角度（0~360）
+    private static double getDegreesYNegativeCW(double x, double y) {
+        double degrees = 90 + getDegreesXNegativeCW(x, y);
+        while (degrees < 0) {
+            degrees += 360;
+        }
+        while (degrees > 360) {
+            degrees -= 360;
+        }
+        return degrees;
+    }
+
+    // 线段与X轴正方向逆时针形成的夹角角度（0~360）
+    private static double getDegreesYPositiveCCW(double x, double y) {
+        double degrees = getDegreesXPositiveCCW(x, y) - 90;
+        while (degrees < 0) {
+            degrees += 360;
+        }
+        while (degrees > 360) {
+            degrees -= 360;
+        }
+        return degrees;
+    }
+
+    // 线段与X轴反方向逆时针形成的夹角角度（0~360）
+    private static double getDegreesYNegativeCCW(double x, double y) {
+        double degrees = getDegreesXNegativeCCW(x, y) - 90;
+        while (degrees < 0) {
+            degrees += 360;
+        }
+        while (degrees > 360) {
+            degrees -= 360;
+        }
+        return degrees;
+    }
+
     /**
-     * 计算在Android坐标系中，以(x1, y1)为原点，从(x1, y1)到(x2, y2)连成线段，
-     * 该线段与X轴正方向顺时针形成的夹角角度（0~360）
+     * 计算在屏幕坐标系中，从(0, 0)到(x, y)连成线段，
+     * 该线段与X或Y轴的正反方向的顺逆时针形成的夹角角度（0~360）
      *
-     * @param x1 x1
-     * @param y1 y1
-     * @param x2 x2
-     * @param y2 y2
-     * @return 夹角
+     * @param xAxis             X轴为true，Y轴为false
+     * @param positiveDirection 正方向为true，反方向为false
+     * @param clockwise         顺时针为true，逆时针为false
+     * @param x                 屏幕坐标系X轴坐标
+     * @param y                 屏幕坐标系Y轴坐标
+     * @return 夹角角度（0~360）
      */
-    public static double getDegrees(double x1, double y1, double x2, double y2) {
-        return getDegrees(x2 - x1, y2 - y1);
+    public static double getDegrees(boolean xAxis, boolean positiveDirection, boolean clockwise,
+                                    double x, double y) {
+        if (xAxis) {
+            // X轴
+            if (positiveDirection) {
+                // X轴正方向
+                if (clockwise) {
+                    // 顺时针
+                    return getDegreesXPositiveCW(x, y);
+                } else {
+                    // 逆时针
+                    return getDegreesXPositiveCCW(x, y);
+                }
+            } else {
+                // X轴反方向
+                if (clockwise) {
+                    // 顺时针
+                    return getDegreesXNegativeCW(x, y);
+                } else {
+                    // 逆时针
+                    return getDegreesXNegativeCCW(x, y);
+                }
+            }
+        } else {
+            // Y轴
+            if (positiveDirection) {
+                // Y轴正方向
+                if (clockwise) {
+                    // 顺时针
+                    return getDegreesYPositiveCW(x, y);
+                } else {
+                    // 逆时针
+                    return getDegreesYPositiveCCW(x, y);
+                }
+            } else {
+                // Y轴反方向
+                if (clockwise) {
+                    // 顺时针
+                    return getDegreesYNegativeCW(x, y);
+                } else {
+                    // 逆时针
+                    return getDegreesYNegativeCCW(x, y);
+                }
+            }
+        }
+    }
+
+    /**
+     * 计算在屏幕坐标系中，从(ox, oy)到(x, y)连成线段，
+     * 该线段与X或Y轴的正反方向的顺逆时针形成的夹角角度（0~360）
+     *
+     * @param xAxis             X轴为true，Y轴为false
+     * @param positiveDirection 正方向为true，反方向为false
+     * @param clockwise         顺时针为true，逆时针为false
+     * @param ox                屏幕坐标系ox
+     * @param oy                屏幕坐标系oy
+     * @param x                 屏幕坐标系x
+     * @param y                 屏幕坐标系y
+     * @return 夹角角度（0~360）
+     */
+    public static double getDegrees(boolean xAxis, boolean positiveDirection, boolean clockwise,
+                                    double ox, double oy, double x, double y) {
+        return getDegrees(xAxis, positiveDirection, clockwise, x - ox, y - oy);
     }
 }
